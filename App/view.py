a43fb761b2ec324a5b027af2fc71bea8170ae1b6
JumaKnav <jm.rodriguezc1@uniandes.edu.co>
2021-02-07 21:28:13 -0500

    Cambios Estudiante 1

--- App/view.py
+++ App/view.py
@@ -43,6 +43,7 @@ def printMenu():
     print("1- Cargar Libros")
     print("2- Cargar Tags")
     # TO-DO: Modificación de Est-1 en el Lab 2
+    print("3- Cargar Book-Tags!")
     print("0- Salir")
 
 
@@ -59,11 +60,13 @@ def loadTags():
     """
     return controller.loadTags('GoodReads/tags.csv')
 
+
 def loadBookTags():
     """
     Cargar los Tags de libros
     """
     # TO-DO: Modificación de Est-1 en el Lab 2
+    return controller.loadBookTags('GoodReads/book_tags-small-csv')
 
 
 """
@@ -78,7 +81,7 @@ while True:
         print('Total de libros cargados: ' + str(lt.size(books)))
 
         # TO-DO: Modificación de Est-1 en el Lab 2
-
+        print('Primer libro cargado: ' + str(lt.firstElement(books)))
 
         # TO-DO: Modificación de Est-2 en el Lab 2
 
